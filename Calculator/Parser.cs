eb7453cdfcd4e5ff27276fa2f0ed8a928b02a259
andriypushkar <andriypushkar@users.noreply.github.com>
2017-02-22 17:14:49 +0200

    Created by Coding Ground Client

---
+++ Calculator/Parser.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using System;
+
+class Parser
+{
+
+    static public Node Parse(string statement)
+    {
+        Console.WriteLine(statement);
+        Stack stack = new Stack();
+        Node root = new Node();
+        
+        
+        
+        return null;
+    }
+    
+}
